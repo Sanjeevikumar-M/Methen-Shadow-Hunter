5376354427553b5cce8db0a3664b519d0c6d982a
Sanjeevikumar M <sanjeevikumar2225@gmail.com>
2026-08-24 15:45:43 +0530

    feat: isolate frontend codebase

--- django_backend/methane_watcher/urls.py
+++
@@ -1,6 +0,0 @@
-"""URL configuration for methane_watcher project."""
-from django.urls import path, include
-
-urlpatterns = [
-    path("api/", include("plume_api.urls")),
-]
--- django_backend/plume_api/management/__init__.py
+++
@@ -1,1 +0,0 @@
-# Initialize management module
